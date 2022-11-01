240f56af5bec8bd154e5d1342d7e41a79f9733c5
sofiaroz29 <sofiarozenberg29@gmail.com>
2022-11-01 15:51:43 -0300

    made pdf with user input

--- backend-dermatos/routes/reporte.routes.js
+++ backend-dermatos/routes/reporte.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import multer from "multer";
 import  Reporte  from '../models/reporte.js';
 import path from "path";
+import {jsPDF} from "jspdf";
 
 const router = Router();
 
@@ -32,11 +33,15 @@ const upload = multer({
 router.post('/upload', upload.array("imagen", 1), async (req,res) =>{
 
     const {parte_del_cuerpo, sintomas, antecedentes, conducta_sol, fototipos} = req.body;
-    
+    console.log(req.files);
     if (!req.files) {
         res.send("File was not found");
         return;
     }
+
+    const imgformat = (req.files[0].mimetype).split('/');
+    console.log(imgformat); 
+
     const newAnalysisRequest = await Reporte.create({
         parte_del_cuerpo,
         sintomas,
@@ -44,6 +49,7 @@ router.post('/upload', upload.array("imagen", 1), async (req,res) =>{
         conducta_sol,
         fototipos,
         imagen: req.files[0].path,
+        imgformat: imgformat[1],
     });
 
     if (!newAnalysisRequest.imagen) {
@@ -55,9 +61,51 @@ router.post('/upload', upload.array("imagen", 1), async (req,res) =>{
         res.json({ message: "Se ha subido correctamente" });
     }
   
-
+    
+        
 });
 
+
+router.get('/analysisresults', async (req,res) =>{ 
+
+    const {usuarioId} = req.body;
+
+    const analysis = await Reporte.findOne({ 
+        where: { usuarioId },
+        order:[ [ 'createdAt', 'DESC' ]],
+    }).catch((err) => {
+        console.log("Error: ", err);
+    });
+   
+    console.log(analysis);
+
+    const doc = new jsPDF();
+    doc.setFontSize(40);
+    doc.setFont("helvetica", "bold");
+    doc.text("Informe", 105, 45, null, null, "center");
+    doc.setFontSize(23);
+    doc.setFont("helvetica", "normal");
+    doc.text("Fecha: " + analysis.createdAt, 30, 80);
+    doc.text("Datos Personales", 30, 95); 
+    doc.setFontSize(20);
+    doc.text("Nombre: ", 30, 106); 
+    doc.text("Apellido: ", 30, 115);
+    doc.text("Email: ", 30, 125);
+    doc.setFontSize(23);
+    doc.text("Evaluacion", 30, 142);
+    doc.setFontSize(20);
+    doc.text("Parte del cuerpo: " + analysis.parte_del_cuerpo, 30, 153); 
+    doc.text("Síntomas: " + analysis.sintomas, 30, 162);
+    doc.text("Antecedentes: " + analysis.antecedentes, 30, 172);
+    doc.text("Conducta respecto al sol: " + analysis.conducta_sol, 30, 182);
+    doc.text("Fototipo: " + analysis.fototipos, 30, 192);
+    doc.setFontSize(23);
+    doc.text("Resultado: " + analysis.estado, 30, 212);
+    doc.addImage(analysis.imagen, analysis.imgformat, 30, 225, 50, 50);
+    
+    doc.save("Analysis-Dermatos-" + Date.now());
+
+});
 
 
 export default router;
