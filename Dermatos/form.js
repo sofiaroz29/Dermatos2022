c8c28ddee054dc17a28556892551bcef65fa557b
LucasRehmann <101809208+LucasRehmann@users.noreply.github.com>
2022-11-06 22:38:51 -0300

    form data transfer

---
+++ Dermatos/form.js
@@ -0,0 +1,19 @@
+function save() {
+    let parte = document.getElementById('parte').value
+    let sint = document.getElementById('sintomas').value
+    let fami = document.getElementById('afamiliares').value
+    let sol = document.getElementById('conductasol').value
+    let foto = document.getElementById('fototipo').value
+    let img = document.getElementById('img').value
+
+    fetch('localhost:3000/upload', {
+        method: 'POST',
+        body: {
+            parte_del_cuerpo: parte,
+            sintomas: sint,
+            antecedentes: fami,
+            conducta_sol: sol,
+            fototipos: foto
+        }
+      });
+} 
