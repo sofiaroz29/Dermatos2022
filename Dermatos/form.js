e6b6647c8ba8d29aa93278ab22e8443db8306ca3
sofiaroz29 <sofiarozenberg29@gmail.com>
2022-11-08 14:49:09 -0300

    continued data transfer

--- Dermatos/form.js
+++ Dermatos/form.js
@@ -4,21 +4,26 @@ function save() {
     let fami = document.getElementById('afamiliares').value
     let sol = document.getElementById('conductasol').value
     let foto = document.getElementById('fototipo').value
-    let img = document.getElementById('img').value
+    let img = document.getElementById('img').files
+
+    let formData = new FormData();
+    formData.append("parte_del_cuerpo", parte);
+    formData.append("sintomas", sint);
+    formData.append("antecedentes", fami);
+    formData.append("conducta_sol", sol);
+    formData.append("fototipos", foto);
+    formData.append("imagen", img);
 
     fetch('http://localhost:3000/api/upload', {
         method: 'POST',
-        headers: {
-         'Content-Type': 'multipart/form-data',
-        },
-        body: {
-            parte_del_cuerpo: parte,
-            sintomas: sint,
-            antecedentes: fami,
-            conducta_sol: sol,
-            fototipos: foto
-        }
+        // headers: {
+        //   'Content-Type': 'multipart/form-data',
+        // },
+        credentials: "include",
+        body: formData,
       });
+
+    console.log(img)
 } 
 
 save();
