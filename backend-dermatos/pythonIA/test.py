f5eaa60cfcc62773e312d5ef6574f15fc8d97879
solparrondo <101563449+solparrondo@users.noreply.github.com>
2022-11-09 15:30:20 -0300

    added test.py script

---
+++ backend-dermatos/pythonIA/test.py
@@ -0,0 +1,42 @@
+import numpy as np
+import tensorflow
+from tensorflow.keras.preprocessing import image
+from keras.applications.resnet import preprocess_input
+
+
+
+def receive_resize_img():    
+    #deberia recibir la imagen entera, este es solo un ejemplo a base de la ruta de una imagen random
+    img = image.load_img('/Users/solparrondo/Documents/dermatos/inputTest/lunarJessTest.jpeg', target_size=(224,224))
+    img_array = image.img_to_array(img)
+    img_batch = np.expand_dims(img_array, axis=0)
+    img_preprocessed = preprocess_input(img_batch)
+    return img_preprocessed
+
+
+def predict_img(img):
+  loaded_model = tensorflow.keras.models.load_model('model.h5')
+  resultado = loaded_model.predict(img)
+  predicted_class= (resultado > 0.5).astype("int32")
+  
+  print('Outputs shape')    
+  print(resultado.shape) # prints (n,1) but  need (n,)
+  resultado=np.transpose(resultado)[0]  # transformation to get (n,)
+  print(resultado.shape)  # now the shape is (n,)
+  # Applying transformation to get binary values predictions with 0.5 as thresold
+  resultado = list(map(lambda x: 0 if x<0.5 else 1, resultado))
+
+  print("----RESULTADO-----")
+  print(resultado)
+
+  print("----CLASS-----")
+  if resultado[0] == 0:
+    print ("maligno")
+  else:
+    print("benigno")  
+  #print([np.argmax(resultado)])
+
+  return resultado
+
+receive_resize_img
+predict_img(img_preprocessed)
