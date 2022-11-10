346bdd1090cf88997d39cb1df987f1352ea8bd46
“solparrondo” <solp2005@gmail.com>
2022-11-10 19:58:49 -0300

    updated flask request for receiving image and analyse it

--- backend-dermatos/pythonIA/flaskserver.py
+++ backend-dermatos/pythonIA/flaskserver.py
@@ -1,19 +1,57 @@
-import numpy as np
-from flask import Flask, request, jsonify
-import pickle
+#import numpy as np
+#from flask import Flask, request, jsonify
+#import pickle
+
+#app = Flask(__name__)
+# Load the model
+#model = pickle.load(open('model.pkl','rb'))
+#@app.route('/api',methods=['POST'])
+
+#def predict():
+    # Get the data from the POST request.
+  #  data = request.get_json(force=True)
+    # Make prediction using model loaded from disk as per the data.
+   # prediction = model.predict([[np.array(data['exp'])]])
+    # Take the first value of prediction
+   # output = prediction[0]
+   # return jsonify(output)
+#if __name__ == '__main__':
+   #    app.run(port=5000, debug=True)
+
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from test.py import receive_resize_img
+from test.py import predict_img
+
+UPLOAD_FOLDER = '/pythonIA/images'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
-# Load the model
-model = pickle.load(open('model.pkl','rb'))
-@app.route('/api',methods=['POST'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+def allowed_file(filename):
+     return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def predict():
-    # Get the data from the POST request.
-    data = request.get_json(force=True)
-    # Make prediction using model loaded from disk as per the data.
-    prediction = model.predict([[np.array(data['exp'])]])
-    # Take the first value of prediction
-    output = prediction[0]
-    return jsonify(output)
+@app.route('/', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            filename = "img_temp."+filename.rsplit('.', 1)[1].lower()
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return predict_img(receive_resize_img(filename))
+        
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+      app.run(port=5000, debug=True)
+    
